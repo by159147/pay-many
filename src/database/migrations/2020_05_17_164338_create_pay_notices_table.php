bac903c6daaf58caa4057690439b3daaa1f131cd
381145264@qq.com <zh6526629>
2020-07-01 19:50:20 +0800

    'Notify'

--- src/database/migrations/2020_05_17_164338_create_pay_notices_table.php
+++ src/database/migrations/2020_05_17_164338_create_pay_notices_table.php
@@ -16,7 +16,7 @@ class CreatePayNoticesTable extends Migration
         Schema::create('pay_notices', function (Blueprint $table) {
             $table->id();
             $table->dateTime('pay_time')->comment('支付时间');
-            $table->string('user')->comment('用户标识');
+            $table->string('user')->nullable()->comment('用户标识');
             $table->string('target_order_id')->comment('第三方订单号');
             $table->string('order_number')->comment('平台订单号');
             $table->integer('buyer_pay_amount')->comment('实付金额');
